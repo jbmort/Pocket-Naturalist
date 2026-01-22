3f4a2da00ee2a468c2cfb38241761b232a218161
Jacob Mortensen <jake.b.mortensen@gmail.com>
2026-01-22 15:25:19 -0600

    initial commit

---
+++ naturalist/src/main/java/com/pocket/naturalist/NaturalistApplication.java
@@ -0,0 +1,13 @@
+package com.pocket.naturalist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaturalistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaturalistApplication.class, args);
+	}
+
+}
---
+++ naturalist/src/test/java/com/pocket/naturalist/NaturalistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pocket.naturalist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaturalistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
