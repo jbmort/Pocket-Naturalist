e495cffd142182d7a0575afe1e12a16f5d1c1da2
Jacob Mortensen <jake.b.mortensen@gmail.com>
2026-02-28 09:08:57 -0600

    created method to authenticate users at log in

--- naturalist/src/main/java/com/pocket/naturalist/controller/LogInController.java
+++ naturalist/src/main/java/com/pocket/naturalist/controller/LogInController.java
@@ -1,5 +1,50 @@
 package com.pocket.naturalist.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pocket.naturalist.dto.JWTAuthResponse;
+import com.pocket.naturalist.dto.LoginDTO;
+import com.pocket.naturalist.security.JwtService;
+
+@RestController
 public class LogInController {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
+
+    public LogInController(
+        AuthenticationManager authenticationManager,
+        JwtService jwtService
+    ){
+        this.authenticationManager = authenticationManager;
+        this.jwtService = jwtService;
+    }
+
+
+    @PostMapping("/login")
+    public ResponseEntity<JWTAuthResponse> authenticateUser(@RequestBody LoginDTO creds){
+            Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        creds.username(),
+                        creds.password()
+                )
+            );
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+
+            String token = jwtService.generateToken((UserDetails) authentication.getPrincipal());
+
+            return ResponseEntity.ok(new JWTAuthResponse(token));
+
+    }
+
     
 }
--- naturalist/src/test/java/com/pocket/naturalist/controllerTests/LogInControllerTest.java
+++ naturalist/src/test/java/com/pocket/naturalist/controllerTests/LogInControllerTest.java
@@ -1,7 +1,14 @@
 package com.pocket.naturalist.controllerTests;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+
+import java.util.ArrayList;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,7 +16,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
@@ -39,6 +50,9 @@ class LogInControllerTest {
     private AuthenticationProvider authenticationProvider;
 
     @MockitoBean
+    private AuthenticationManager authenticationManager;
+
+    @MockitoBean
     private JwtService jwtService;
 
     @MockitoBean(name = "parkSecurity") 
@@ -57,11 +71,21 @@ class LogInControllerTest {
     @WithMockUser(username = "username", password = "password")
     void shouldAllowUserToLogIn() throws Exception{
         LoginDTO logInCreds = new LoginDTO("username", "password");
+        String expectedToken = "eyJhbGciOiJIUzI1NiJ9.mockTokenValue";
+
+
+        Authentication mockAuth = mock(Authentication.class);
+        UserDetails mockUser = new User("username", "password", new ArrayList<>());
+
+        when(mockAuth.getPrincipal()).thenReturn(mockUser);
+        when(authenticationManager.authenticate(any())).thenReturn(mockAuth);
+        when(jwtService.generateToken(mockUser)).thenReturn(expectedToken);
 
         mockMvc.perform(post("/login")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(logInCreds)))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.accessToken").value(expectedToken));
 
     }
     
