bc007d77633a0d09c633a024f8b398ece31cb372
Jacob Mortensen <jake.b.mortensen@gmail.com>
2026-03-06 18:34:10 -0600

    update checkin controller tests to handle security

--- naturalist/src/test/java/com/pocket/naturalist/controllerTests/CheckInControllerTest.java
+++ naturalist/src/test/java/com/pocket/naturalist/controllerTests/CheckInControllerTest.java
@@ -11,10 +11,12 @@ import org.locationtech.jts.geom.Polygon;
 import org.n52.jackson2.datatype.jts.JtsModule;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.pocket.naturalist.controller.CheckInController;
@@ -24,59 +26,62 @@ import com.pocket.naturalist.entity.Park;
 import com.pocket.naturalist.repository.UserRepository;
 import com.pocket.naturalist.security.JwtService;
 import com.pocket.naturalist.security.ParkSecurity;
+import com.pocket.naturalist.security.SecurityConfig;
 import com.pocket.naturalist.service.LocationService;
+import com.pocket.naturalist.service.UserService;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest(controllers = CheckInController.class)
-class CheckInControllerTest {
-    Point userLocation;
-    Polygon parkBoundary;
-    Park park;
 
+@WebMvcTest(controllers = CheckInController.class)
+@Import(SecurityConfig.class)
+class CheckInControllerTest {
 
     GeometryFactory geometryFactory;
+    Park park;
+    Point userLocation;
     ObjectMapper objectMapper;
 
+    @MockitoBean
+    private UserDetailsService userDetailsService; 
+
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    // --- MOCKS ---
     @MockitoBean
     private JwtService jwtService;
 
     @MockitoBean(name = "parkSecurity") 
     private ParkSecurity parkSecurity;
 
+    @MockitoBean
+    private AuthenticationProvider authenticationProvider;
     
     @MockitoBean
     private UserRepository userRepository; 
 
     @MockitoBean
-    LocationService locationService;
+    private UserService userService;
 
-    @Autowired
-	private MockMvc mockMvc;
+    @MockitoBean
+    private LocationService locationService;
 
-    @Autowired
-    WebApplicationContext webApplicationContext;
-
-    @SuppressWarnings("removal")
     @BeforeEach
     void setUp() {
+        objectMapper = new ObjectMapper().registerModule(new JtsModule());
         geometryFactory = new GeometryFactory();
-        parkBoundary = geometryFactory.createPolygon(geometryFactory.createLinearRing(new Coordinate[] {
-                new Coordinate(0, 0),
-                new Coordinate(2, 0),
-                new Coordinate(2, 2),
-                new Coordinate(0, 2),
-                new Coordinate(0, 0)
+        Polygon parkBoundary = geometryFactory.createPolygon(geometryFactory.createLinearRing(new Coordinate[] {
+                new Coordinate(0, 0), new Coordinate(2, 0), new Coordinate(2, 2), new Coordinate(0, 2), new Coordinate(0, 0)
         }), null);  
         park = new Park("Test Park");
         park.addBoundary(parkBoundary);
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
-        this.objectMapper = new ObjectMapper().registerModule(new JtsModule());
     }
-    
-
 
     @Test
+    @WithMockUser
     void checkInShouldReturnPositiveWhenUserInsideParkBoundary() throws Exception {
         userLocation = geometryFactory.createPoint(new Coordinate(1, 1));
         when(locationService.isPointInsideParkBoundaries(userLocation, park.getUrlSlug())).thenReturn(true);
@@ -92,6 +97,7 @@ class CheckInControllerTest {
     }
 
     @Test
+    @WithMockUser
     void checkInShouldReturnNegativeWhenUserOutsideParkBoundary() throws Exception {
         userLocation = geometryFactory.createPoint(new Coordinate(3, 3));
         when(locationService.isPointInsideParkBoundaries(userLocation, park.getUrlSlug())).thenReturn(false);
@@ -108,6 +114,7 @@ class CheckInControllerTest {
     }
 
     @Test
+    @WithMockUser
     void verifyVicinityOfParkFeature() throws Exception {
         userLocation = geometryFactory.createPoint(new Coordinate(1.1, 1.2));
         int featureId = 1;
@@ -125,6 +132,7 @@ class CheckInControllerTest {
     }
 
     @Test
+    @WithMockUser
     void verifyNonVicinityOfParkFeature() throws Exception {
         userLocation = geometryFactory.createPoint(new Coordinate(5, 5));
         int featureId = 1;
