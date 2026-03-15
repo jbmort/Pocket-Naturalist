35eb6a947bb0ac86ce98ccc66afb7c10875ed0c3
Jacob Mortensen <jake.b.mortensen@gmail.com>
2026-03-15 17:04:06 -0500

    update sighting service test for new repository method

--- naturalist/src/test/java/com/pocket/naturalist/serviceTests/SightingServiceTest.java
+++ naturalist/src/test/java/com/pocket/naturalist/serviceTests/SightingServiceTest.java
@@ -3,6 +3,8 @@ package com.pocket.naturalist.serviceTests;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.when;
 
 import java.util.List;
@@ -70,7 +72,7 @@ class SightingServiceTest {
         Park park = new Park("Yellowstone");
         park.setAnimals(Set.of(animal));
 
-        when(sightingsRepository.findAllByPark(park))
+        when(sightingsRepository.findSightingsForToday(eq(park), any()))
         .thenReturn(List.of(
             new Sighting(animal, locationOfAnimal, locationOfReport, park)));
 
